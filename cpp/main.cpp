88a5b8afe4a3536cc4285fb7ed8dbc735d782b94
Jonathan J. Greenhoe <dgreenhoe@gmail.com>
2020-01-05 00:50:12 +0000

    number game with levels

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -4,38 +4,59 @@
 #include <stdlib.h>
 #include <time.h>
 #include "main.h"
-//void jjfunction(void);
 void numberGame(void);
 
 int main(void)
 {
   numberGame();
 }
-// this is a number game where you have 5 chances to guess the right number.
+// this is a number game where you have 8 chances to guess the right number.
 void numberGame(void)
 {
   int n;
   int key;
   int randNum;
+  int L;
+  char keystring[20];
   srand(time(NULL));
   printf("this is a number game ");
-  printf("where you have 5 chances to guess the right number.\n");
-  randNum=rand()%10+'0'; 
-for (n=5; n>=1; n-=1)
+  printf("where you have 8 chances to guess the right number.%d\n",key);
+for(L=1; L<=10&&n>1; L++)
   {
-    if(n==1)
-      printf("enter a number between 1 and 10. you have 1 guess left\n");
-    else
-      printf("enter a number between 1 and 10. you have %d guesses left\n", n);
-    key=getchar();
-    fflush(stdin);
-    if (key==randNum)
+  printf("Level %d\n",L);
+  randNum=rand()%100;
+for (n=8; n>=1; n-=1)
+  {
+  if(n==1)
+    printf("enter a number between 0 and 9. you have 1 guess left\n");
+  else
+    printf("enter a number between 0 and 9. you have %d guesses left\n", n);
+  gets(keystring);
+  sscanf(keystring,"%d",&key);
+  printf("you entered the %d key and ",key);
+  if(key==randNum)
     {
-      printf("and you are right!");
+      printf("you are right!\n");
+      n=2;
+      if(L>=10)
+      printf("you won!");
       break;
     }
     else
-      printf("and you are wrong! HA! HA! HA!\n");
+      {
+      if(n!=1)
+      if(key>randNum)
+      printf("that is too high! Guess lower.");
+      if(key<randNum)
+      printf("that is too low!guess higher.");
+      if(n==1)
+        {
+      printf("\nGame over\n");
+      break;
+      }
+        }
+     }
   }
-  printf("the correct answer is %c!",randNum);
+    if(key!=randNum)
+  printf("the correct answer is %d!",randNum);
 }
