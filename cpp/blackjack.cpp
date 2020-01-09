2076bf23178689b284d8284523ef6a3487d89b3b
Jonathan J. Greenhoe <dgreenhoe@gmail.com>
2020-01-09 10:56:34 +0000

    update

--- cpp/blackjack.cpp
+++ cpp/blackjack.cpp
@@ -3,52 +3,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-// this is a number game where you have 8 chances to guess the right number.
+int drawCard(void)
+{
+  int randNum;
+  randNum=rand()%12+1;
+  return randNum;
+}
 void blackjack(void)
 {
   int n;
   int key;
-  int randNum;
+  int prandNum;
+  int crandNum;
+  int csum=0;
+  int psum=0;
   int L;
+  int s;
   srand(time(NULL));
   printf("this is a blackjack game ");
   printf("where you compete againsts the computer to get closest to 21.\n");
 for(L=1; L<=10&&n>1; L++)
   {
   printf("Level %d\n",L);
-  randNum=rand()%10;
-  for (n=1; n>=1; n+=1)
+  prandNum=rand()%12+1;
+  crandNum=rand()%12+1;
+
+  for (n=1; n++;);
   {
   if(n==1)
-    printf("enter 'h' for hit me to draw a card.\n");
+    printf("enter 'h' for to draw a card.\n");
   else
-    printf("enter 'h' for hit me to draw another card. enter 's' for stop to stop drawing\n\n", n);
+    printf("enter 'h' for hit me to draw another card. enter 's' to stop drawing\n");
   key=getchar();
   if(key=='h')
     {
-      printf("you drew a %d card.\n",randNum);
-      n=2;
-      if(L>=10)
-      printf("you won!");
-      break;
-    }
-    else
-      {
-      if(n!=1)
-      {
-      if(key>randNum)
-      printf("that is too high! Guess lower.");
-      if(key<randNum)
-      printf("that is too low!guess higher.");
-      }
-      if(n==1)
-        {
-      printf("you are wrong.\nGame over\n");
-      break;
-      }
-        }
-     }
+      prandNum=drawCard();
+      crandNum=drawCard();
+      psum+=prandNum;
+      csum+=crandNum;
+      printf("you drew a %d card.you now have %d points\n",prandNum ,psum);
+      printf("your opponent drew a %d card.He now has %d points\n",crandNum ,csum);
+  if(psum>21)
+  {
+  printf("you lose.");
+  break;
   }
-    if(key!=randNum)
-  printf("the correct answer is %d!",randNum);
-}
+    if(csum>21)
+    printf("you have completed Level %d.",L);
+    if(L>=10)
+    {
+    printf("you won!");
+    break;
+    }
+    }
+  if(key==s)
+  if(csum<=psum)
+  drawCard();
+  }
+  }
+  }
