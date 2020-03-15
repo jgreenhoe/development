8dd3dfad893776201d3bb0fcbdaaad995b7c1155
Jonathan J. Greenhoe <dgreenhoe@gmail.com>
2020-03-15 12:42:43 +0000

    blackjack working

--- cpp/blackjack.cpp
+++ cpp/blackjack.cpp
@@ -11,7 +11,7 @@ int drawCard(void)
 }
 void blackjack(void)
 {
-  int n;
+  int n=-1;
   int key;
   int prandNum;
   int crandNum;
@@ -20,45 +20,129 @@ void blackjack(void)
   int L;
   int s;
   srand(time(NULL));
-  printf("this is a blackjack game ");
-  printf("where you compete againsts the computer to get closest to 21.\n");
-for(L=1; L<=10&&n>1; L++)
+  printf("blackjack\npress i for instructions or c to continue.\n");
+  key=getchar();
+  fflush(stdin);
+  if(key=='i')
   {
+    printf("you and the computer will take turns to draw cards.\n");
+    printf("each card will be between 1 and 13. you and the computer are competing to be the closest to 21.\n");
+    printf("but don't go over, or you lose.\n");
+    printf("if you think that you are close enough to 21, you can stop drawing,\n");
+    printf("and the computer can continue drawing. the opposite is also true.\n");
+    printf("this game has 10 levels, and you win when they are completed.\n");
+    key='c';
+  }
+if(key=='c')
+for(L=1; (L<=11) && (n<0); L++)
+{
+  if(L==11)
+  {
+    printf("you won!");
+    break;
+  }
+  csum=0;
+  psum=0;
+  n=2;
   printf("Level %d\n",L);
   prandNum=rand()%12+1;
   crandNum=rand()%12+1;
-
-  for (n=1; n++;);
+  for (n=2; n>1; n++)
   {
-  if(n==1)
-    printf("enter 'h' for to draw a card.\n");
-  else
-    printf("enter 'h' for hit me to draw another card. enter 's' to stop drawing\n");
-  key=getchar();
-  if(key=='h')
+    if(n==2)
+      printf("enter 'h'to draw a card.\n");
+    else
+      printf("enter 'h'to draw another card. enter 's' to stop drawing\n");
+    key=getchar();
+    fflush(stdin);
+    if(key=='h')
     {
       prandNum=drawCard();
       crandNum=drawCard();
-      psum+=prandNum;
-      csum+=crandNum;
-      printf("you drew a %d card.you now have %d points\n",prandNum ,psum);
-      printf("your opponent drew a %d card.He now has %d points\n",crandNum ,csum);
-  if(psum>21)
-  {
-  printf("you lose.");
-  break;
-  }
-    if(csum>21)
-    printf("you have completed Level %d.",L);
-    if(L>=10)
-    {
-    printf("you won!");
-    break;
+      if(csum>=psum && (csum-psum)<(21-csum) || psum>csum)
+      {
+        csum+=crandNum;
+        printf("your opponent drew a %d card.He now has %d points\n",crandNum ,csum);
+        if(csum>21)
+        {
+          printf("you have completed Level %d.\n",L);
+          n=-2;
+          break;
+        }
+        if(csum<21)
+        {
+          psum+=prandNum;
+          printf("you drew a %d card.you now have %d points\n",prandNum ,psum);
+          if(psum>21)
+          {
+            printf("you lose");
+            n=0;
+            break;
+          }
+        }
+      }
+      else
+      {
+        printf("your opponent has stopped drawing.would you like to keep drawing?\n");
+        while(n>1)
+        {
+          key=getchar();
+          fflush(stdin);
+          if(key=='h')
+          {
+            prandNum=drawCard();
+            psum+=prandNum;
+            if(psum>21)
+            {
+              printf("you drew a %d card.you now have %d points.\nyou lose\n",prandNum,psum,n);
+              n=0;
+              break;
+            }
+            printf("you drew a %d card.you now have %d points.\n",prandNum,psum);
+            if(psum<=csum)
+              printf("would you like to keep drawing?\n");
+          }
+            if(key=='s' && csum>=psum && csum<=21)
+            {
+              printf("you lose");
+              n=0;
+              key='1';
+              break;
+            }
+          if(psum>csum)
+          {
+            printf("you have completed Level %d.\n",L);
+            n=-2;
+            break;
+          }
+        }
+      }
     }
-    }
-  if(key==s)
-  if(csum<=psum)
-  drawCard();
+      if(key=='s' && csum>psum && csum<=21)
+      {
+        printf("you lose");
+        n=0;
+        break;
+      }
+      if(key=='s')
+      while(csum<=psum && csum<=21 && n>1)
+      {
+        crandNum=drawCard();
+        csum+=crandNum;
+        printf("your opponent drew again.he drew a %d card. he now has %d points\n",crandNum,csum);
+        if(csum>psum && csum<=21 || psum>21)
+        {
+          printf("you lose");
+          n=0;
+          break;
+        }
+      if(csum>21 && psum<=21)
+      {
+        printf("you have completed Level %d.\n",L);
+        n=-2;
+        break;
+      }
+      }
   }
-  }
-  }
+}
+}
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,10 +1,38 @@
-
 #include "main.h"
+void mainMenu(void);
 void blackjack(void);
 void numberGame(void);
-
+void codeBreaker(void);
+void videoGame(void);
+void tmp(void);
+int main(void);
+int key;
 int main(void)
 {
-  blackjack();
+  mainMenu();
 }
-
+void mainMenu(void)
+{
+  printf("please choose a game\n");
+  printf("press 1 for secret number\n");
+  printf("press 2 for blackjack\n");
+  printf("press 3 for code breaker\n");
+  printf("press 4 for video game\n");
+  key=getchar();
+  fflush(stdin);
+  switch(key)
+  {
+    case'1':
+      numberGame();
+      break;
+    case'2':
+      blackjack();
+      break;
+    case'3':
+      codeBreaker();
+      break;
+    case'4':
+      videoGame();
+      break;
+  }
+}
