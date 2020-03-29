5f06c10ed93db4fcea0e808bea8c97d302cf6553
Jonathan J. Greenhoe <dgreenhoe@gmail.com>
2020-03-29 18:59:20 +0000

    automatically moves

--- cpp/PDcurses38/demos/jj.c
+++ cpp/PDcurses38/demos/jj.c
@@ -3,6 +3,8 @@
 void seas(void);
 void greet(void);
 void del_msg(void);
+void plyrCntrl(void);
+void path(void);
 void jerase(int row,int col);
 void ball(int row, int col);
 
@@ -31,39 +33,18 @@ int main(int argc, char **argv)
     refresh();
     napms(1000);
 
-    int key;
-    int row=0;
-    int col=12;
-    int oldrow=row;
-    int oldcol=col;
     seas();
     greet();
     del_msg();
     napms(1000);
-    while(1)
-    {
-       key=0;
-       key=GetCh();
-       oldrow=row;
-       oldcol=col;
-       switch(key)
-       {
-         case'w': row-=2; break;
-         case's': row+=2; break;
-         case'd': col+=2; break;
-         case'a': col-=2; break;
-         case'x': exit(0);
-       }
-    printf("%c",key);
-       jerase(oldrow,oldcol);
-       ball(row,col);
-    }
+    path();
+  
     napms(1000);
 
     napms(3000);
     return 0;
-}
 
+}
 void ball(int row, int col)
 {
     mvwaddch(screen, row, col, (chtype) 'J');
@@ -102,6 +83,47 @@ void del_msg(void)
     refresh();
 }
 
+void plyrCntrl(void)
+{
+  int key;
+  int row=35;
+  int col=0;
+  int oldrow=row;
+  int oldcol=col;
+  while(1)
+  {
+     key=GetCh();
+     oldrow=row;
+     oldcol=col;
+     switch(key)
+     {
+       case'w': row-=2; break;
+       case's': row+=2; break;
+       case'd': col+=2; break;
+       case'a': col-=2; break;
+       case'x': exit(0);
+     }
+     jerase(oldrow,oldcol);
+     ball(row,col);
+  }
+}
+void path(void)
+{
+  int row=40;
+  int col=0;
+  int oldrow=row;
+  int oldcol=col;
+  while(row<90)
+  {
+    oldrow=row;
+    oldcol=col;
+    row-=2;
+    col+=2;
+    napms(500);
+    jerase(oldrow,oldcol);
+    ball(row,col);
+  }
+}
 // http://www.cplusplus.com/forum/articles/19975/
 CHAR GetCh (VOID)
 {
