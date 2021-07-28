0d2537796f0bd6e2abdf9bd5cf329a888771ba8a
Jonathan J. Greenhoe <dgreenhoe@gmail.com>
2021-07-27 20:49:54 -0400

    Start stock analysis

---
+++ stats/mmp.R
@@ -0,0 +1,44 @@
+#============================================================================
+# Daniel J. Greenhoe
+# R script file
+# setwd("c:/jj/stats/");
+# source("dice.R");
+#============================================================================
+#---------------------------------------
+# install packages (perform once)
+#---------------------------------------
+#install.packages("bspec");
+#install.packages("ramify");
+#---------------------------------------
+# load add-on packages
+#---------------------------------------
+ rm(list=objects());
+ require(stats);
+ require(graphics);
+ require(datasets);
+ require(ramify);
+ require(bspec);     # https://www.rdocumentation.org/packages/bspec/
+#---------------------------------------
+# load data
+#---------------------------------------
+ x    = read.csv("mmp.csv", comment.char = "#");
+ bins=seq(from=0.5,to=12.5,by=1)
+ Open = x$Open;
+ Close = x$Close;
+ Date = x$Date;
+ #plot( bins,Red, col="red", type='o' );
+ #lines( bins,Blue, col="blue", type='o' );
+ hist(Close)
+#---------------------------------------
+# plot data
+#---------------------------------------
+# mydata(x);
+# estT = mypsd(x, numSegments=3, dataDump=FALSE);
+# print(sprintf("estimated period = %.16f years", estT));
+
+
+#mypdf(x);
+#pdfr(x);
+#---------------------------------------
+# process data
+#---------------------------------------
